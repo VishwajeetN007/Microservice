5b489d730cbe7a7ba5560ae5e63fb788892841c6
VishwajeetN007 <vishwajeetnikam@gmail.com>
2024-07-19 00:25:54 +0530

    Docker compose and Docker Swarm

---
+++ WebApp-Container/WebAppContainer/WebAppContainer/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppContainer.Database;
+using WebAppContainer.Database.Entities;
+
+namespace WebAppContainer.Controllers
+{
+    public class UserController : Controller
+    {
+        AppDbContext _db;
+        public UserController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _db.Users.ToList();
+            return View(users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Users.Add(model);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApp-Container/WebAppContainer/WebAppContainer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppContainer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
