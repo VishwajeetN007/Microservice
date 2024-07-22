4ba0a8bb2f79f4203d8b60b45c3fe28829feb43c
VishwajeetN007 <vishwajeetnikam@gmail.com>
2024-07-22 17:24:03 +0530

    Kubernetes for Web app and Sql server

---
+++ Kubernetes/WebAppContainerK8S/WebAppContainer/Database/SQL-Schema/Schema.sql
@@ -0,0 +1,23 @@
+﻿USE [MyDb]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 18-07-2024 20:47:24 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](100) NOT NULL,
+	[Email] [varchar](100) NOT NULL,
+	[Address] [varchar](300) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
