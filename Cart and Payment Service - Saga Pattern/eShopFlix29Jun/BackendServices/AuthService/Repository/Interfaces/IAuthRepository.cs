efb8e7f2a477cf45116df3a94fff6242698aaa26
VishwajeetN007 <vishwajeetnikam@gmail.com>
2024-07-28 22:17:50 +0530

    Auth Service changes

---
+++ Cart and Payment Service - Saga Pattern/eShopFlix29Jun/BackendServices/AuthService/Repository/Interfaces/IAuthRepository.cs
@@ -0,0 +1,10 @@
+﻿using AuthService.Models;
+
+namespace AuthService.Repository.Interfaces
+{
+    public interface IAuthRepository
+    {
+        bool CreateUser(SignUpModel signUpModel);
+        UserModel ValidateUser(LoginModel loginModel);
+    }
+}
---
+++ Cart and Payment Service - Saga Pattern/eShopFlix29Jun/BackendServices/CartService/CartService/HttpClients/CatalogService.cs
@@ -0,0 +1,19 @@
+﻿using CartService.Models;
+
+namespace CartService.HttpClients
+{
+    public sealed class CatalogService
+    {
+        HttpClient _client;
+        public CatalogService(HttpClient httpClient)
+        {
+            _client = httpClient;
+        }
+
+        public async Task<ProductModel> GetProductAsync(int id)
+        {
+            var products = await _client.GetFromJsonAsync<ProductModel>("product/get/"+id);
+            return products ?? null;
+        }
+    }
+}
