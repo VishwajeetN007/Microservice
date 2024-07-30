a3ccc1e7097ba88620a7d2d1df0d85041aafb5b8
VishwajeetN007 <vishwajeetnikam@gmail.com>
2024-07-31 01:20:25 +0530

    Added
    Added

---
+++ Cart and Payment Service - Saga Pattern/eShopFlix29Jun/SupportApp/SupportApp/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SupportApp.HttpClients;
+
+namespace SupportApp.Areas.Admin.Controllers
+{
+    public class ProductController : BaseController
+    {
+        ProductService _productService;
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAll();
+            return View(products);
+        }
+    }
+}
---
+++ Cart and Payment Service - Saga Pattern/eShopFlix29Jun/SupportApp/SupportApp/Helpers/BaseViewPage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using SupportApp.Models;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace SupportApp.Helpers
+{
+    public abstract class BaseViewPage<TModel> : RazorPage<TModel>
+    {
+        public UserModel CurrentUser
+        {
+            get
+            {
+                //Get the ClaimsPrincipal for user associated with the executing action.
+                if (User.Claims.Count() > 0)
+                {
+                    string userdata = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.UserData).Value;
+                    return JsonSerializer.Deserialize<UserModel>(userdata);
+                }
+                return null;
+            }
+        }
+    }
+}
