5d80c7f70395ce8a2198a72a07764fa77b4f8b76
VishwajeetN007 <vishwajeetnikam@gmail.com>
2024-07-31 01:49:52 +0530

    Support App added
    Support App added

---
+++ Cart and Payment Service - Saga Pattern/eShopFlix29Jun/FrontendServices/SupportApp/SupportApp/HttpClients/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using SupportApp.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace SupportApp.HttpClients
+{
+    public class AuthService
+    {
+        HttpClient _client;
+        public AuthService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<UserModel> Login(LoginModel loginModel)
+        {
+            StringContent content = new StringContent(JsonSerializer.Serialize(loginModel), Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("auth/login", content);
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var user = JsonSerializer.Deserialize<UserModel>(data);
+                return user;
+            }
+            return null;
+        }
+    }
+}
---
+++ Cart and Payment Service - Saga Pattern/eShopFlix29Jun/FrontendServices/SupportApp/SupportApp/HttpClients/BaseService.cs
@@ -0,0 +1,28 @@
+﻿using SupportApp.Models;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace SupportApp.HttpClients
+{
+    public class BaseService
+    {
+        IHttpContextAccessor _httpContextAccessor;
+
+        public BaseService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public UserModel CurrentUser
+        {
+            get
+            {
+                if (_httpContextAccessor.HttpContext.User.Claims.Count() > 0)
+                {
+                    string userdata = _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.UserData).Value;
+                    return JsonSerializer.Deserialize<UserModel>(userdata);
+                }
+                return null;
+            }
+        }
+    }
+}
