f705dcb4ad134e951fd8647d79bda4eea4363f7b
VishwajeetN007 <vishwajeetnikam@gmail.com>
2024-07-31 01:41:57 +0530

    Payment Service added
    Payment Service added

---
+++ Cart and Payment Service - Saga Pattern/eShopFlix29Jun/BackendServices/PaymentService/Models/PaymentConfirmModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PaymentService.Models
+{
+    public class PaymentConfirmModel
+    {
+        public string PaymentId { get; set; }
+        public string OrderId { get; set; }
+        public string Signature { get; set; }
+    }
+}
---
+++ Cart and Payment Service - Saga Pattern/eShopFlix29Jun/BackendServices/PaymentService/Services/Interfaces/IPaymentRepository.cs
@@ -0,0 +1,14 @@
+﻿using PaymentService.Database.Entities;
+using PaymentService.Models;
+using Razorpay.Api;
+
+namespace PaymentService.Services.Interfaces
+{
+    public interface IPaymentRepository
+    {
+        string CreateOrder(RazorPayOrder order);
+        Payment GetPaymentDetails(string paymentId);
+        bool SavePaymentDetails(PaymentDetail model);
+        string VerifyPayment(PaymentConfirmModel payment);
+    }
+}
