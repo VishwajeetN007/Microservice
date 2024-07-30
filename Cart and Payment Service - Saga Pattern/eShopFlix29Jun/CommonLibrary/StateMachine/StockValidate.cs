7821e1e4663b767a3bc0a0ccf9f446645664ca2a
VishwajeetN007 <vishwajeetnikam@gmail.com>
2024-07-31 01:13:07 +0530

    Added
    Added

---
+++ Cart and Payment Service - Saga Pattern/eShopFlix29Jun/CommonLibrary/StateMachine/StockValidate.cs
@@ -0,0 +1,18 @@
+﻿using CommonLibrary.Database;
+using CommonLibrary.Messages.Commands;
+
+namespace CommonLibrary.StateMachine
+{
+    public class StockValidate : IStockValidate
+    {
+        private readonly OrderState orderSaga;
+        public StockValidate(OrderState orderState)
+        {
+            orderSaga = orderState;
+        }
+        public Guid OrderId => orderSaga.OrderId;
+        public string PaymentId => orderSaga.PaymentId;
+        public string Products => orderSaga.Products;
+        public long CartId => orderSaga.CartId;
+    }
+}
